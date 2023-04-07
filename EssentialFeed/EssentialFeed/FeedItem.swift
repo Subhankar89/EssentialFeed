267d5b40ddcde107293cc75d8787d6e5290197e6
Subhankar <subhankaracharya@gmail.com>
2023-04-07 12:04:52 +0100

    Crete the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Subhankar Acharya on 07/04/2023.
+//
+
+import Foundation
+
+struct Item {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
