565881a2c711da7acfa9a3481d65ede5c69cf93a
Subhankar <subhankaracharya@gmail.com>
2024-04-08 10:20:06 +0530

    Extract system under test creation to a factory method to protect changes under breaking changes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -37,8 +37,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
     // Does Not delete the cached upon creation
     func test_init_doesNotDeleteCacheUponCreation() {
         
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
@@ -46,9 +45,9 @@ final class CacheFeedUseCaseTests: XCTestCase {
     // calling the save command
     func test_save_requestsCacheDeletion() {
         
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
         let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        
         sut.save(items)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
@@ -56,12 +55,19 @@ final class CacheFeedUseCaseTests: XCTestCase {
     
     // MARK: - Helpers
     
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        return (sut, store)
+    }
+    
     private func uniqueItem() -> FeedItem {
         FeedItem(id: UUID(), description: "any", 
                  location: "any", imageURL: anyURL())
     }
     
     private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
+        URL(string: "http://any-url.com")!
     }
 }
