f4634bffd13303fdd1482f7c997eee6a7795b466
Subhankar <subhankaracharya@gmail.com>
2024-05-02 11:10:00 +0530

    Retrieving after inserting to empty cache delivers inserted values

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // FeedImage representation for the Cache Module
-public struct LocalFeedImage: Equatable {
+public struct LocalFeedImage: Equatable, Codable {
     public let id: UUID
     public let description: String?
     public let location: String?
