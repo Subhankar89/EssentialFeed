f0f47932567cd405a4c63a3a54ed2a9480deff00
Subhankar <subhankaracharya@gmail.com>
2024-05-08 16:29:55 +0530

    Minor changes to fix warning

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -116,7 +116,7 @@ class URLSessionHTTPClientTests: XCTestCase {
                                  file: StaticString = #filePath, line: UInt = #line) -> (data: Data, response: HTTPURLResponse)? {
         let result = resultFor(data: data, response: response, error: error, file: file, line: line)
         switch result {
-        case let .success(data, response):
+        case let .success((data, response)):
             return (data, response)
         default:
             XCTFail("Expected success, got \(String(describing: result)) instead", file: file, line: line)
