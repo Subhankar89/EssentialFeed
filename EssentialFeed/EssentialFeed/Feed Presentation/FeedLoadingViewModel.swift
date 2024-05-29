b1f1791db8000bc6c4f76350b20adf13016f0327
Subhankar <subhankaracharya@gmail.com>
2024-05-29 11:46:20 +0530

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Subhankar  Acharya on 29/05/24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
