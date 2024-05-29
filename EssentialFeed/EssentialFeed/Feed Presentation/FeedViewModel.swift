bc70895897fe21d4f2301b8bd3f87c74a9af5dc6
Subhankar <subhankaracharya@gmail.com>
2024-05-29 11:45:16 +0530

    Move FeedViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Subhankar  Acharya on 29/05/24.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
