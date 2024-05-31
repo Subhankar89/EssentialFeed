dbaff248a31e4d85e96c8677d991daa4b92bbe94
Subhankar <subhankaracharya@gmail.com>
2024-05-31 14:43:20 +0530

    Add new FeedStore core data model version adding an optional Data p… …roperty to the `ManagedFeedImage`

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,12 +8,13 @@
 import CoreData
 
 @objc(ManagedFeedImage)
- class ManagedFeedImage: NSManagedObject {
-    @NSManaged  var id: UUID
-    @NSManaged  var imageDescription: String?
-    @NSManaged  var location: String?
-    @NSManaged  var url: URL
-    @NSManaged  var cache: ManagedCache
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var data: Data?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
 
 extension ManagedFeedImage {
