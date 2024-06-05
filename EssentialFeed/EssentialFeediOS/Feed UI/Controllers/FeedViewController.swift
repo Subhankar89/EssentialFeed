f417f6942c384ac051eecff5a7b0080de74e865d
Subhankar <subhankaracharya@gmail.com>
2024-06-05 10:17:31 +0530

    Intercept HTTP requests with canned responses during UI tests to elim… …inate network flakiness - We can now run UI tests without internet connection.

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -73,11 +73,15 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(forRowAt: indexPath).cancelLoad()
+        cellControllers[indexPath]?.cancelLoad()
+        cellControllers[indexPath] = nil
     }
     
     private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
-        tableModel[indexPath.row]
+        let controller = tableModel[indexPath.row]
+        cellControllers[indexPath] = controller
+        
+        return controller
     }
     
     private func removeCellController(forRowAt indexPath: IndexPath) {
