3b0ea6237007e297ad0b92ea950ab0470f7bad8c
Subhankar <subhankaracharya@gmail.com>
2024-06-08 21:44:47 +0530

    Move Combine helpers to a new file

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -13,17 +13,17 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-
+    
     private lazy var httpClient: HTTPClient = {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
     private lazy var store: FeedStore & FeedImageDataStore = {
-         try! CoreDataFeedStore(
-             storeURL: NSPersistentContainer
-                 .defaultDirectoryURL()
-                 .appendingPathComponent("feed-store.sqlite"))
-     }()
+        try! CoreDataFeedStore(
+            storeURL: NSPersistentContainer
+                .defaultDirectoryURL()
+                .appendingPathComponent("feed-store.sqlite"))
+    }()
     
     private lazy var localFeedLoader: LocalFeedLoader = {
         LocalFeedLoader(store: store, currentDate: Date.init)
@@ -41,13 +41,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func configureWindow() {
- 
-        
         window?.rootViewController = UINavigationController(
             rootViewController: FeedUIComposer.feedComposedWith(
                 feedLoader: makeRemoteFeedLoaderWithLocalFallback ,
                 imageLoader: makeLocalImageLoaderWithRemoteFallback))
-        
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
@@ -66,119 +63,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
-            let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
-            let localImageLoader = LocalFeedImageDataLoader(store: store)
-
-            return localImageLoader
-                .loadImageDataPublisher(from: url)
-                .fallback(to: {
-                    remoteImageLoader
-                        .loadImageDataPublisher(from: url)
-                        .caching(to: localImageLoader, using: url)
-                })
-        }
-}
-
-
-
-public extension FeedImageDataLoader {
-     typealias Publisher = AnyPublisher<Data, Error>
-
-     func loadImageDataPublisher(from url: URL) -> Publisher {
-         var task: FeedImageDataLoaderTask?
-
-         return Deferred {
-             Future { completion in
-                 task = self.loadImageData(from: url, completion: completion)
-             }
-         }
-         .handleEvents(receiveCancel: { task?.cancel() })
-         .eraseToAnyPublisher()
-     }
- }
-
-extension Publisher where Output == Data {
-     func caching(to cache: FeedImageDataCache, using url: URL) -> AnyPublisher<Output, Failure> {
-         handleEvents(receiveOutput: { data in
-             cache.saveIgnoringResult(data, for: url)
-         }).eraseToAnyPublisher()
-     }
- }
-
- private extension FeedImageDataCache {
-     func saveIgnoringResult(_ data: Data, for url: URL) {
-         save(data, for: url) { _ in }
-     }
- }
-
-public extension FeedLoader {
-    
-    typealias Publisher = AnyPublisher<[FeedImage], Error>
-    
-    func loadPublisher() -> Publisher {
-        Deferred {
-            Future(self.load)
-        }
-        .eraseToAnyPublisher()
-    }
-}
-
-extension Publisher {
-    func fallback(to fallbackPublisher: @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
-        self.catch { _ in fallbackPublisher() }.eraseToAnyPublisher()
-    }
-}
-
-extension Publisher where Output == [FeedImage] {
-    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
-        handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
-    }
-}
-
-private extension FeedCache {
-    func saveIgnoringResult(_ feed: [FeedImage]) {
-        save(feed) { _ in }
-    }
-}
-
-extension Publisher {
-    func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
-        receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
-    }
-}
-
-extension DispatchQueue {
-    
-    static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
-        ImmediateWhenOnMainQueueScheduler()
-    }
-    struct ImmediateWhenOnMainQueueScheduler: Scheduler {
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+        let localImageLoader = LocalFeedImageDataLoader(store: store)
         
-        typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
-        
-        typealias SchedulerOptions = DispatchQueue.SchedulerOptions
-        
-        var now: SchedulerTimeType {
-            DispatchQueue.main.now
-        }
-        
-        var minimumTolerance: SchedulerTimeType.Stride {
-            DispatchQueue.main.minimumTolerance
-        }
-        
-        func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard Thread.isMainThread else {
-                return DispatchQueue.main.schedule(options: options, action)            }
-            
-            action()
-        }
-        
-        func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
-            DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
-        }
-        
-        func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) ->  Cancellable {
-            DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
-        }
+        return localImageLoader
+            .loadImageDataPublisher(from: url)
+            .fallback(to: {
+                remoteImageLoader
+                    .loadImageDataPublisher(from: url)
+                    .caching(to: localImageLoader, using: url)
+            })
     }
 }
