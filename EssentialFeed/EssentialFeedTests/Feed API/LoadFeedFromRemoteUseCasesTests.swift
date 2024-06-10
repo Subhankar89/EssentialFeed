fb530baa3ac51fb12a47539857a606debf14cc8d
Subhankar <subhankaracharya@gmail.com>
2024-06-10 11:41:38 +0530

    Replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCasesTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCasesTests.swift
@@ -10,44 +10,6 @@ import EssentialFeed
 
 class LoadFeedFromRemoteUseCasesTests: XCTestCase {
     
-    func test_init_doesNotRequestedDataFromURL() {
-        // create FeedLoader with a client
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        // control over which URL to request from
-        let url = URL(string: "https://a-given-url.com")!
-        
-        let (sut, client) = makeSUT(url: url)
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        // control over which URL to request from
-        let url = URL(string: "https://a-given-url.com")!
-        
-        let (sut, client) = makeSUT(url: url)
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: failure(.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            // completion happens, after invoke load
-            client.complete(with: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon200HTTPClientError() {
         let (sut, client) = makeSUT()
         
@@ -96,19 +58,6 @@ class LoadFeedFromRemoteUseCasesTests: XCTestCase {
             let json = makeItemJSON([item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json)
         })
-    }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://any-given-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-        
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut?.load { capturedResults.append($0) }
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemJSON([]))
-        XCTAssertTrue(capturedResults.isEmpty)
     }
     
     // MARK: Helpers - factory method
