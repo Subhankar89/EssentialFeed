4f466dba3e4ccec6b6dc59947641e6acada6f7f7
Subhankar <subhankaracharya@gmail.com>
2024-06-28 16:48:16 +0530

    Extract helper methods to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -70,6 +70,19 @@ extension ListViewController {
     var errorMessage: String? {
         return errorView.message
     }
+    
+    func numberOfRows(in section: Int) -> Int {
+        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
+    }
+    
+    func cell(row: Int, section: Int) -> UITableViewCell? {
+        guard numberOfRows(in: section) > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: section)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
 }
 
 extension ListViewController {
@@ -124,26 +137,19 @@ extension ListViewController {
     }
     
     func numberOfRenderedFeedImageViews() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
+        numberOfRows(in: feedImagesSection)
     }
     
-    private var feedImagesSection: Int {
-        0
-    }
+    private var feedImagesSection: Int { 0 }
     
     func feedImageView(at row: Int) -> UITableViewCell? {
-        guard numberOfRenderedFeedImageViews() > row else {
-            return nil
-        }
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        return ds?.tableView(tableView, cellForRowAt: index)
+        cell(row: row, section: feedImagesSection)
     }
 }
 
 extension ListViewController {
      func numberOfRenderedComments() -> Int {
-         tableView.numberOfSections == 0 ? 0 :  tableView.numberOfRows(inSection: commentsSection)
+         numberOfRows(in: commentsSection)
      }
 
      func commentMessage(at row: Int) -> String? {
@@ -159,15 +165,8 @@ extension ListViewController {
      }
 
      private func commentView(at row: Int) -> ImageCommentCell? {
-         guard numberOfRenderedComments() > row else {
-             return nil
-         }
-         let ds = tableView.dataSource
-         let index = IndexPath(row: row, section: commentsSection)
-         return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+         cell(row: row, section: commentsSection) as? ImageCommentCell
      }
 
-     private var commentsSection: Int {
-         return 0
-     }
+    private var commentsSection: Int { 0 }
  }
